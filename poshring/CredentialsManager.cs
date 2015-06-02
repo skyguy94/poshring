351ee751215b9898ff13ddc0840822e1e7812b33
skyguy94 <skyguy94@gmail.com>
2015-06-02 18:09:37 -0500

    Return empty result when there are no credentials on the system.

--- poshring/CredentialsManager.cs
+++ poshring/CredentialsManager.cs
@@ -14,7 +14,17 @@ namespace poshring
             IntPtr ptr;
             if (!UnsafeAdvapi32.CredEnumerateW(null, 0x1, out count, out ptr))
             {
-                throw new Win32Exception(Marshal.GetLastWin32Error());
+                var error = (CredentialErrors) Marshal.GetLastWin32Error();
+                switch (error)
+                {
+                    case CredentialErrors.NotFound:
+                        return Enumerable.Empty<Credential>();
+                    case CredentialErrors.NoSuchLogonSession:
+                    case CredentialErrors.InvalidFlags:
+                        throw new Win32Exception((int)error);
+                    default:
+                        throw new InvalidOperationException("Unexpected error while fetching credentials.");
+                }
             }
 
             var credentials = Enumerable.Range(0, count)
